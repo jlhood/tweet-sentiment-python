31a3b18b251c3c06bbc93e1a21041721c4b607af
James Hood <james@jlhood.com>
2019-03-28 09:45:04 -0700

    Improved observability and drill-down
    * Include CW Metrics Dashboard
    * Emit structured logs for CW Insights querying

--- src/tweetsentiment.py
+++ src/tweetsentiment.py
@@ -31,12 +31,35 @@ def handler(tweets, context):
         LanguageCode='en'
     )
 
+    _log_sentiment_results(tweets, sentiment_result['ResultList'])
+
     metric_data = _to_metric_data(sentiment_result)
     LOG.debug('Putting metric data: %s', metric_data)
     CLOUDWATCH.put_metric_data(
         Namespace='TweetSentiment',
         MetricData=metric_data
     )
+
+
+def _log_sentiment_results(tweets, sentiment_result_list):
+    """Log sentiment results in a structured way so it can be queried using CloudWatch Insights."""
+    sentiment_result_lookup = {result['Index']: result for result in sentiment_result_list}
+
+    for i, tweet in enumerate(tweets):
+        sentiment_result = sentiment_result_lookup.get(i)
+        if sentiment_result:
+            LOG.info(
+                'Tweet Sentiment Result: tweet_url:"%s" Positive:%.8f Negative:%.8f Neutral:%.8f Mixed:%.8f',
+                _tweet_url(tweet),
+                sentiment_result['SentimentScore']['Positive'],
+                sentiment_result['SentimentScore']['Negative'],
+                sentiment_result['SentimentScore']['Neutral'],
+                sentiment_result['SentimentScore']['Mixed']
+            )
+
+
+def _tweet_url(tweet):
+    return 'https://twitter.com/{}/status/{}'.format(tweet['user']['screen_name'], tweet['id_str'])
 
 
 def _to_metric_data(sentiment_result):
